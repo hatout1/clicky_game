9c2f5208b31459bc8d0bfbf6ead5a18d6f334445
hatout01 <hatout1@gmail.com>
2020-02-19 17:08:20 -0800

    updated App.js file function to react component and add the constructor function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import './App.css';
-import Nav from './components/navbar/Nav';
-import GameBody from './components/GameBody/GameBody';
-import GameTitle from './components/title/GameTitle';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
+import "./App.css";
+import Nav from "./components/navbar/Nav";
+import GameBody from "./components/GameBody/GameBody";
+import GameTitle from "./components/title/GameTitle";
 
-      <Nav />
-      <GameTitle />
-      <GameBody />
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      score: 0,
+      topScore: 0
+    };
+  }
 
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <Nav />
+        <GameTitle />
+        <GameBody />
+      </div>
+    );
+  }
 }
 
 export default App;
